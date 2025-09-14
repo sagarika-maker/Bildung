0882a9552a96fe149dcb0d47cadf1d1238d73b60
Sai Bhaskar Kandula <137374965+SaiBhaskar0987@users.noreply.github.com>
2025-09-14 12:48:41 +0530

    Add files via upload

--- quizzes/views.py
+++ quizzes/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from .models import Quiz, Question, Choice, StudentAnswer
+from .models import Quiz, Question, Choice, StudentAnswer,  QuizResult
 from courses.models import Course
 
 @login_required
